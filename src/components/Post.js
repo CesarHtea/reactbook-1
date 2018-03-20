1056cadb581716acefccadcf88b2d5ddbd9827e1
Yair <hello@agzeri.com>
2018-03-20 12:55:36 -0600

    [Post.js] Add a class to image to control size.

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,7 +6,7 @@ class Post extends Component {
       <div className='post'>
         <header className='post__header'>
           <div>
-            <img src='https://scontent.fmex11-1.fna.fbcdn.net/v/t1.0-1/p100x100/26904508_10156429668511323_5866309676974531495_n.png?oh=0860b50dc03a86826bc8c2b0d4af61c7&oe=5B28EA9A'/>
+            <img className='post__image' src='https://scontent.fmex11-1.fna.fbcdn.net/v/t1.0-1/p100x100/26904508_10156429668511323_5866309676974531495_n.png?oh=0860b50dc03a86826bc8c2b0d4af61c7&oe=5B28EA9A'/>
           </div>
           <div>
             <h3 className='post__title'>The Guardian</h3>
