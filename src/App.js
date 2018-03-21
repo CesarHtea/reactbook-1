835c1410f8413427eab3a7b6e4356e21cd68b7dd
Yair <hello@agzeri.com>
2018-03-21 12:13:32 -0600

    [App] Import twit library and print config object.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Post from './components/Post';
 
+import Twit from 'twit';
+
+import config from './config';
+
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    console.log(config)
     return (
       <div>
         <Header />
