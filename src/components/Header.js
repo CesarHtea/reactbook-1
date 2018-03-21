d705e80d48cabd79266e0d661272d2b6ac00309a
Yair <hello@agzeri.com>
2018-03-21 09:44:09 -0600

    [Header] Add Logo and Form Markup in Component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <h3>Header</h3>
+    return (
+      <nav className='grid grid--justify header'>
+        <div className='grid'>
+          <img className='header__image' src='https://10s7rk1sjlds1cylpm4fav81-wpengine.netdna-ssl.com/wp-content/uploads/2016/09/480px-Facebook_icon_2013.svg_.png' />
+          <form>
+            <div className='form'>
+              <input type='text' placeholder='Search' />
+              <button>
+                <i className='fa fa-search'></i>
+              </button>
+            </div>
+          </form>
+        </div>
+        <div></div>
+      </nav>
+    );
   }
 }
 
