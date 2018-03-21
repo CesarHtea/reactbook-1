8018f6f85c5f9ad94fc881e8cd851c1aff2e71d6
Yair <hello@agzeri.com>
2018-03-21 12:14:13 -0600

    [Keys] Add Twitter API Keys.

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+  consumer_key:         'D4wZ33Ad8kif0RVCxMKTjJLgs',
+  consumer_secret:      'Ps295Z98fzaWv8uGa0CbzynHZQZiuJDbB6y2iL0r2l97C7PVJc',
+  access_token:         '169319552-xSRx9D3Ikwvs1bTinWZOvS3BjJo5M4xkWgq6RgCX',
+  access_token_secret:  'fe9FSZvsJjdfdLddN8yMs9SdIsTEcZ5uBqTkQ53BJUl46',
+};
