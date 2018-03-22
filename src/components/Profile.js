a6f6df376c68dd60b903966b60f53f1b8469b057
Yair <hello@agzeri.com>
2018-03-22 16:03:32 -0600

    [Profile] Add basic structure to Profile.

---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return (
+      <h2>This is my Profile</h2>
+    );
+  }
+}
+
+export default Profile;
