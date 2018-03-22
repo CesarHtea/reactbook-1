4ec82dfe514b851885a37a9452e87c87acca3e29
Yair <hello@agzeri.com>
2018-03-22 16:16:34 -0600

    [Home] Make an API request and render Post components.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import request from 'superagent';
+
+import Post from './Post';
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    request
+      .get('https://randomuser.me/api/?results=20')
+      .then(response => {
+        this.setState({
+          data: response.body.results
+        })
+      });
+  }
+
   render() {
     return (
-      <h2>This is HOME.</h2>
+      <div>
+        {this.state.data.map(function(p, i) {
+          return <Post key={i} info={p} />
+        })}
+      </div>
     );
   }
 }
